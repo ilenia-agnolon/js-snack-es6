1babebf48ad36fd2448e454561855758e6fd0885
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-09 15:49:55 +0200

    inserito array biciclette e passaggi logici

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
+
+//Snack Array e Oggetti
+
+
 /*
-Snack Array e Oggetti
-
-
 • Snack 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.
+*/
 
+//creo un array che contiene oggetti "bici da corsa" -> ogni oggetto ha 2 proprietà: nome e peso
+const biciclette = [
+  {
+    nome: "Bianchi Sprint",    // nome della bici
+    peso: 7.2                  // peso in kg
+  },
+  {
+    nome: "Specialized Tarmac",
+    peso: 6.8
+  },
+  {
+    nome: "Cannondale SuperSix",
+    peso: 7.0
+  },
+  {
+    nome: "Trek Emonda",
+    peso: 6.7
+  }
+]
+
+
+//1. Inizializzare la variabile
+let biciConPesoMinore = biciclette [0];
+
+//2. Ciclare tutto l'array con for
+
+//3. Stampare in console per vedere quale bici è la più leggera
+
+
+
+/*
 • Snack2
 Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
